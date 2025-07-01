da8c3ce0e177f5a62a8e07535086a88660fb17c2
Mohan Paramasivam <34168793+mohansiva98@users.noreply.github.com>
2025-07-01 22:43:44 +0530

    Update etl_job.py

--- etl_job.py
+++ etl_job.py
@@ -2,8 +2,8 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("ETLJob").getOrCreate()
 
-df = spark.read.csv("s3://etl-raw-data-yourname/sample.csv", header=True, inferSchema=True)
+df = spark.read.csv("s3://etl-raw-data-mohan/sample.csv", header=True, inferSchema=True)
 
 df_clean = df.dropna()
 
-df_clean.write.csv("s3://etl-processed-data-yourname/cleaned", header=True)
+df_clean.write.csv("s3://etl-processed-data-mohan/cleaned", header=True)
